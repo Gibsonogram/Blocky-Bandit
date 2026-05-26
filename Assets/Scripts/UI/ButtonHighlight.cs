833cd462dc52a0a88a56ead15ff7be6345b635c9
Gibsonogram <gibsonadamsc@gmail.com>
2026-05-25 17:54:11 -0700

    world1 and world2 transitioning with levelselect flow and button highlight

--- Assets/Scripts/UI/ButtonHighlight.cs
+++ Assets/Scripts/UI/ButtonHighlight.cs
@@ -7,4 +7,6 @@ public class ButtonHighlight : MonoBehaviour, ISelectHandler, IDeselectHandler
 
     public void OnSelect(BaseEventData eventData) => highlightGraphic.SetActive(true);
     public void OnDeselect(BaseEventData eventData) => highlightGraphic.SetActive(false);
+    private void OnEnable() { if (EventSystem.current != null && EventSystem.current.currentSelectedGameObject == gameObject) highlightGraphic.SetActive(true); }
+    private void OnDisable() => highlightGraphic.SetActive(false);
 }
